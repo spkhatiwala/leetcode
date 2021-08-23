2e4fb41f6f2e380b89fa648081466f2362610a92
Siddharth <spkhatiwala@yahoo.com>
2021-08-23 17:31:11 -0400

    description

--- FloydWarshall.java
+++ FloydWarshall.java
@@ -1,18 +1,55 @@
 public class FloydWarshall {
+
+    //Finds shortest path for all vertices.
+    //works with -ve weights also
+    //go through each vertex of the graph.
+    //and recalculate the whole graph by passing through that
+    //vertex.....
+
+
     final static int INF = Integer.MAX_VALUE, V = 4;
 
     void floydWarshall(int graph[][]){
-        for(int i=1; i<=V; i++){
-            for(int row=1; row<=V; row++){
-                for(int col=1; col<=V; col++){
+        int dist[][] = new int[graph.length][graph[0].length];
 
+        //just copy the matrix... Not really needed.
+        for(int i=0; i<graph.length; i++){
+            for(int j=0; j<graph[0].length; j++){
+                dist[i][j] = graph[i][j];
+            }
+        }
+
+
+
+
+        for(int v=0; v<V; v++){
+            for(int row=0; row<V; row++){
+                for(int col=0; col<V; col++){
+                    if(dist[row][v]!=INF && dist[v][col]!=INF)
+                        dist[row][col] = Math.min(dist[row][col], dist[row][v] + dist[v][col]);
                 }
             }
         }
+        printSolution(dist);
     }
 
 
-
+    void printSolution(int dist[][])
+    {
+        System.out.println("The following matrix shows the shortest "+
+                "distances between every pair of vertices");
+        for (int i=0; i<V; ++i)
+        {
+            for (int j=0; j<V; ++j)
+            {
+                if (dist[i][j]==INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j]+"   ");
+            }
+            System.out.println();
+        }
+    }
 
 
     // Driver program to test above function
